da7f68eb6eeef64a9adb2a588fd49dc7356f745b
mtttia <tia369852@gmail.com>
2022-05-03 23:18:17 +0200

    added IP address in db_crud for local

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const  config = {
     user:  'sa',
     password:  'Burbero2020',
-    server:  '192.168.1.229', // 'PC0118' // '10.1.0.18' // '192.168.43.48'
+  server:  '192.168.43.48', // 'PC0118' // '10.1.0.18' // '192.168.43.48' // '192.168.1.229'
     database:  'Bar_5G',
     options: {
       trustedconnection:  true,
@@ -12,4 +12,4 @@ const  config = {
     port:  1433
   }
   
-  module.exports = config;
+  module.exports = config;  
